6049eaaf40b992e96c2ffdf340b31c93f71426d1
Sanja Huhle <sanja.huhle@gmail.com>
2020-05-05 02:14:50 +0200

    Add a test that tests the bug

--- src/services/weka-library.service.spec.ts
+++ src/services/weka-library.service.spec.ts
@@ -1,5 +1,6 @@
 import {WekaLibraryService} from './weka-library.service';
 import {RandomForestContainer} from '../model/random-forest-container.model';
+import * as fs from 'fs';
 
 describe('WekaLibraryService', () => {
 
@@ -28,6 +29,13 @@ describe('WekaLibraryService', () => {
     test('should return the file path of an balanced file with ending', async () => {
         const result: string = await serviceUnderTest.getTrainingFilePathBalanced('test_dataset.arff');
         expect(result).toEqual('input\\datasets\\balanced\\test_dataset.arff');
+    });
+
+    test('should balance all datasets', async () => {
+        await serviceUnderTest.balanceAllDataSets();
+        const fileContent: string = await fs.readFileSync(await serviceUnderTest.getTrainingFilePathBalanced('test_dataset.arff'), {encoding: 'utf-8'});
+        const fileContentPerLine: string[] = fileContent.split('\n');
+        expect(fileContentPerLine[0]).toEqual('@relation CustomDataset');
     });
 
 });
