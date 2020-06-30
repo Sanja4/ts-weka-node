f0cf225cfe0052b785ce304467ce90bdbfab0cf6
Martin Wende <martin.wende@lhind.dlh.de>
2020-06-30 15:45:17 +0200

    Update j48 options documentation

--- src/model/options/j48-options.model.ts
+++ src/model/options/j48-options.model.ts
@@ -1,6 +1,9 @@
 export class J48Options {
+    /** Whether pruning should be used or not. False = pruning is used. True = pruning is not used. Default is false. */
     U?: boolean = false;
+    /** Minimum number of instances per leaf. Default is 2. */
     M?: number = 2;
+    /** confidenceFactor - The confidence factor used for pruning (smaller values incur more pruning). Default is 0.25 */
     C?: number = 0.25;
 
     constructor(params?: J48Options) {
